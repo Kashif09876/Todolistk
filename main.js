32a8cc643cc9520f6072e92d673757bc9f086af1
Kashif khan <159754234+Kashif09876@users.noreply.github.com>
2024-05-12 19:40:08 +0500

    Add files via upload
    Todo Create by Kashif khan

---
+++ main.js
@@ -0,0 +1,58 @@
+import inquirer from "inquirer";
+// inquirer 
+// array
+// function
+// operators
+let todos = ["Shoaib", "Zain"];
+async function createTodo(todos) {
+    do {
+        let ans = await inquirer.prompt({
+            type: "list",
+            message: "select an operation",
+            name: "select",
+            choices: ["Add", "update", "view", "remove"],
+        });
+        if (ans.select == "Add") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "add items in the list",
+                name: "todo",
+            });
+            todos.push(addTodo.todo);
+            console.log(todos);
+        }
+        if (ans.select == "update") {
+            let updatetodo = await inquirer.prompt({
+                type: "list",
+                message: "update items in the list",
+                name: "todo",
+                choices: todos.map(item => item)
+            });
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "add items in the list",
+                name: "todo",
+            });
+            let newtodo = todos.filter(val => val !== updatetodo.todo);
+            todos = [...newtodo, addTodo.todo];
+            console.log(todos);
+        }
+        if (ans.select == "view") {
+            console.log("*** TO DO LIST ***");
+            console.log(todos);
+            console.log("********");
+        }
+        if (ans.select == "remove") {
+            let removetetodo = await inquirer.prompt({
+                type: "list",
+                message: "update items in the list",
+                name: "todo",
+                choices: todos.map(item => item)
+            });
+            let newtodo = todos.filter(val => val !== removetetodo.todo);
+            todos = [...newtodo];
+            console.log(todos);
+        }
+    } while (true);
+}
+createTodo(todos);
